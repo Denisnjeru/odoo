499c5accf35e18e612d0894b29ef556eea158a55
Your Name <you@example.com>
2024-03-18 00:34:14 +0300

    estate addon

---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'estate',
+    'version': '1.0',
+    'category': 'Sales/estate',
+    'sequence': 15,
+    'summary': 'Training estate module',
+    'description': "",
+    'website': 'https://www.odoo.com/page/estate',
+    'depends': ['base', 'mail'],
+    'installable': True,
+    'application': True,
+    # 'auto_install': False
+}
